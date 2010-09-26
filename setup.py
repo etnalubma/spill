19e0fd9795f26e5c0445a936b9045aa8450211a2
Matias Iturburu <maturburu@gmail.com>
2010-09-26 18:03:12 -0300

    The great divide

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='spill',
+    version='0.8dev',
+    author=u'Francisco Herrero',
+    author_email='francisco.herrero@gmail.com',
+    url='http://github.com/etnalubma/spill',
+    description = 'Herramienta de linea de comandos apra publicar en ltmo',
+)
+
